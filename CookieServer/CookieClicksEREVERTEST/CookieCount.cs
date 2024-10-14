5ca85db63a9c947299376974a1c2d52d6d50ff02
siemh <siemhorst0@gmail.com>
2024-10-14 14:03:43 +0200

    test add server

---
+++ CookieServer/CookieClicksEREVERTEST/CookieCount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieCount
+{
+    public class cookie
+    {
+        public int COOKIES { get; set; }
+
+
+
+        public cookie(int cookie)
+        {
+            this.COOKIES = cookie;
+        }
+
+            public void addcookies()
+            {
+                this.COOKIES++;
+            } 
+       
+ 
+        public override string ToString()
+        {
+            return "Cookies: " + COOKIES;
+        }
+
+    }
+}
